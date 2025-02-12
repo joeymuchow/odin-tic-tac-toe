4661426f5ef914adc374940d31f69f5245af3c67
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-02-11 16:15:03 -0800

    Complete console tictactoe game
    The tictactoe game is now finished as a console game. The entire game
    is inside the tictactoe module with an IIFE that only exposes the
    startGame function and the two functions for each player to make a move.

--- tictactoe.js
+++ tictactoe.js
@@ -4,30 +4,106 @@ const tictactoe = (() => {
         return { board };
     })();
 
+    const gameState = {
+        turn: "",
+        round: 0,
+        winner: "",
+    };
+
+    const updateGameState = (marker) => {
+        if (!checkForGameOver(marker)) {
+            gameState.round += 1;
+            changeTurn();
+            render();
+        } else {
+            render();
+            const message = gameState.winner === "tie" ? "It's a tie." : `Congrats ${gameState.winner}`;
+            console.log(message);
+        }
+    };
+
+    const checkForGameOver = (marker) => {
+        let gameOver = false;
+
+        const playerMoves = [];
+        const winningCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+        gameboard.board.forEach((val, index) => {
+            if (val === marker) {
+                playerMoves.push(index);
+            }
+        });
+
+        for (const combo of winningCombos) {
+            const isWinner = checkForCombo(combo, playerMoves);
+            if (isWinner) {
+                gameOver = true;
+                gameState.winner = marker === players.player1.marker ? players.player1.name : players.player2.name;
+                break;
+            }
+        }
+
+        if (gameState.round === 9 && !gameOver) {
+            gameState.winner = "tie";
+            gameOver = true;
+        }
+
+        return gameOver;
+    };
+
+    const checkForCombo = (combo, playerMoves) => {
+        for (const num of combo) {
+            if (!playerMoves.includes(num)) {
+                return false;
+            }
+        }
+        return true;
+    };
+
     const PlayerFactory = (name, marker) => {
-        const playPiece = (marker, spot) => {
-            board[spot] = marker;
+        const playPiece = (spot) => {
+            if (gameState.turn === name) {
+                if (gameboard.board[spot] === "-") {
+                    gameboard.board[spot] = marker;
+                    updateGameState(marker);
+                } else {
+                    console.log("That space is already taken. Try again.");
+                }
+            } else {
+                console.log(`It is not your turn ${name}!`);
+            }
+            
         }
         return { name, marker, playPiece };
-    }
-    
-    const gameState = {
-        player1: {},
-        player2: {},
-        turn: "",
-        winner: ""
+    };
+
+    const players = {
+        player1: PlayerFactory("player1", "x"),
+        player2: PlayerFactory("player2", "o")
     };
 
     const startGame = () => {
-        gameboard.board.push(null,null,null,null,null,null,null,null,null);
-        const player1 = PlayerFactory("player1", "x");
-        const player2 = PlayerFactory("player2", "o");
-        gameState.player1 = player1;
-        gameState.player2 = player2;
-        gameState.turn = player1;
-    }
+        gameboard.board = ["-","-","-","-","-","-","-","-","-"];
+        gameState.round = 1;
+        gameState.turn = players.player1.name;
+        gameState.winner = "";
+        render();
+    };
 
-    const render = () => {}
+    const changeTurn = () => {
+        gameState.turn = gameState.turn === players.player1.name
+            ? players.player2.name
+            : players.player1.name;
+    };
 
-    return { startGame, player1Move: gameState.player1.playPiece, player2Move: gameState.player2.playPiece };
+    const render = () => {
+        console.log(`Round ${gameState.round}`);
+        console.log(gameboard.board[0],gameboard.board[1],gameboard.board[2]);
+        console.log(gameboard.board[3],gameboard.board[4],gameboard.board[5]);
+        console.log(gameboard.board[6],gameboard.board[7],gameboard.board[8]);
+        if (!gameState.winner) {
+            console.log(`It is now ${gameState.turn}'s turn.`);
+        }
+    };
+
+    return { startGame, player1Move: players.player1.playPiece, player2Move: players.player2.playPiece };
 })();
